a9515358d8f016415f91611bbe5f36892d8ae8ac
Shih-Han Huang <shihhan.huang@gmail.com>
2016-10-11 15:10:42 +0200

    4.5 Views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
+
   def index
+  	@products = Product.all
   end
 
   def landing_page
-  	@featured_product = Product.first
+  	@products = Product.all
   end 
+
 end
