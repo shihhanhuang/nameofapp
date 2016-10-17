c776247919fa839e3a46db753fe513d4cd36ee5b
Shih-Han Huang <shihhan.huang@gmail.com>
2016-10-17 14:05:31 +0200

    5.3 Emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,14 @@ class StaticPagesController < ApplicationController
   	@products = Product.all
   end 
 
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'shihhan.huang@gmail.com',
+  		:subject => "A new contact form message from #{@name}",
+  		:body => @message).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  post 'static_pages/thank_you'
+
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
