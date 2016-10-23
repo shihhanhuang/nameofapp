4483b079ed1dee122de9edd26a6610da0c024c5f
Shih-Han Huang <shihhan.huang@gmail.com>
2016-10-23 16:58:12 +0200

    5.7 Comment framework

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
-class Product < ApplicationRecord
+class Product < ActiveRecord::Base
 	has_many :orders
+	has_many :comments
+
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
 end
