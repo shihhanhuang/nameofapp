468f4ad2a49c4637aac1f7d2f96615dccd96b332
Shih-Han Huang <shihhan.huang@gmail.com>
2016-10-24 16:34:16 +0200

    5.8 Comment Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
-  resources :users, :products
+
+  resources :products do
+  	resources :comments
+  end
+	resources :users
 
   root 'static_pages#index'
 
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
 
 
   resources :orders, only: [:index, :show, :create, :destroy]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
