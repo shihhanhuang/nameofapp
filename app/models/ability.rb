4b3fff657c436cc5ac7efaf0eaa96c22b119f971
Shih-Han Huang <shihhan.huang@gmail.com>
2016-10-26 14:05:13 +0200

    5.10 Admin Role & Moderating

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,15 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+        can :manage, :all
+    else
+        can :read, :all
+        can :create, :all
+        can :update, :all
+    end
+
+    # original: can :manage, User, id: user.id
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
