5faaf23cdbac0b59daafee4b86614088179b74d4
Shih-Han Huang <shihhan.huang@gmail.com>
2016-11-15 16:23:27 +0100

    6.12.2 Redis changed application file

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,12 @@ Bundler.require(*Rails.groups)
 module Nameofapp
   class Application < Rails::Application
 
+  	config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
 
-config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,2 @@
-if Rails.env.production?
-	$redis = Redis.new(url: ENV["REDIS_URL"])
-else
-	$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = Redis.new(url: ENV["REDIS_URL"]) if Rails.env.production?
+	$redis = Redis.new(:host => 'localhost', :port => 6379) if Rails.env.development?
