026ef993c1da7e772ddb762b275acf38faa7dc84
Shih-Han Huang <shihhan.huang@gmail.com>
2016-11-16 16:52:16 +0100

    6.14 ActiveJob and Advanced ActionCable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,6 @@ class Comment < ActiveRecord::Base
   	validates :product, presence: true
   	validates :rating, numericality: { only_integer: true }
 
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) }
+
 end
